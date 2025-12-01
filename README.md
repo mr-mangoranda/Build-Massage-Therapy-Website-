726216eecfce97df5dd5f6b209552f939ffe353c
mr-mangoranda <habibmangoranda15@gmail.com>
2025-12-01 22:01:21 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Build-Massage-Therapy-Website-" 
---
+++ script.js
@@ -0,0 +1,46 @@
+// SHOW MENU
+
+let line = document.querySelector('.navbar .ri-menu-line');
+let menu = document.querySelector('.navbar ul');
+
+line.addEventListener('click', () => {
+    menu.classList.toggle('showmenu');
+});
+
+window.addEventListener('scroll',() =>{
+    let nav = document.querySelector('.navbar');
+    if(scrollY > 50){
+        nav.classList.add('navbarSticky')
+    }
+    else{
+        nav.classList.remove('navbarSticky')
+    };
+});
+
+
+// SERVICE SLIDER
+
+var swiper = new Swiper('.ServiceSwiper', {
+    loop:true,
+    autoplay:{
+        delay:3000,
+    },
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+            spaceBetween:10,
+        },
+        640: {
+            slidesPerView:2,
+            spaceBetween:10,
+        },
+        1024:{
+            slidesPerView:2,
+            spaceBetween:10,
+        },
+        1400:{
+            slidesPerView:4,
+            spaceBetween:10,
+        }
+    }
+})
